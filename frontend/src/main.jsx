66b5737ed1880750add22ed7c605475ad07ff6ae
Rohit Kumar <rohit2004r82@gmail.com>
2026-05-19 00:04:53 +0530

    tanstack and axios setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ClerkProvider } from "@clerk/react";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "./index.css";
 import App from "./App.jsx";
@@ -12,12 +13,16 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk publishable key");
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </ClerkProvider>
   </StrictMode>,
 );
